d82068a4571976808acbda4b7e1d2d3113a8b83f
“ProbieZY <1107254498@qq.com>
2016-10-13 19:28:54 +0800

    edit01

---
+++ DogRaceDay/DogRaceDay/Greyhound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogRaceDay
+{
+    class Greyhound
+    {
+        //起始位置
+        public int StartingPosition;
+        //赛道长度
+        public int RacetrackLength;
+        //PictureBox对象
+        public PictureBox MyPictureBox=null;
+        //赛道上的位置
+        public int Location = 0;
+        //随机距离对象
+        public Random 
+    }
+}
